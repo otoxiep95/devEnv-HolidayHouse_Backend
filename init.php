97e7731e572a52ea480a66ce7361b6adeb761909
Lars <you@example.com>
2020-05-19 13:09:41 +0200

    fixed untracked files

--- init.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once(__DIR__ . '/utils/functions.php');
-require_once(__DIR__ . '/utils/Database.php');
-require_once(__DIR__ . '/utils/ApiResponse.php');
-require_once(__DIR__ . '/utils/Auth.php');
